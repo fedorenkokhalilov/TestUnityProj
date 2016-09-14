61c7021882446c70168e3030fdbc324e77780269
User-ПК\User <fedorenkokhalilov@gmail.com>
2016-09-14 20:34:36 +0300

    Add script

---
+++ Test/Assets/Scripts/CimitMesendger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CimitMesendger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("first commit");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
