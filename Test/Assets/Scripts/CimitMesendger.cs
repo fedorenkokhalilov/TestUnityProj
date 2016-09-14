825b4850c9994e3bbf5411d954377441e5c957b5
User-ПК\User <fedorenkokhalilov@gmail.com>
2016-09-14 20:37:44 +0300

    Added second commit

--- Test/Assets/Scripts/CimitMesendger.cs
+++ Test/Assets/Scripts/CimitMesendger.cs
@@ -6,6 +6,7 @@ public class CimitMesendger : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		print ("first commit");
+		print ("second commit");
 	}
 	
 	// Update is called once per frame
